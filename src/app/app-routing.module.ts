3ad61836697f10d6c2f444d19b6d087e4d4bfe28
suresh <sureshkathroj@gmail.com>
2021-03-06 12:55:09 +0530

    added components,service and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component:MenuComponent
+  },
+  {
+    path:'menu',component:MenuComponent
+  },
+  {
+    path:"recipe",component:RecipeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FavouritesComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
